5a1e5546476257682d2a165542729b1017dc7d38
Rodrigo Francesco Bustos Mendieta <168384097+FrancescoMendieta@users.noreply.github.com>
2026-03-13 20:34:37 +0000

    esskeetit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="next-steps">
+    <><section id="next-steps">
         <div id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
